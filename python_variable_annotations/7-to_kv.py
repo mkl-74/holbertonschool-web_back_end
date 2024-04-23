0856c539cc21e4d3554bdfcc6d772e8585dbb31d
mkl-74 <mickael1008@gmail.com>
2024-04-23 13:03:38 +0000

    task7

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""take a string as arguments and return a tuple"""
+
+
+def to_kv(key: str, value: float) -> tuple:
+    """take a string as arguments and return a tuple"""
+    return (key, value**2)
