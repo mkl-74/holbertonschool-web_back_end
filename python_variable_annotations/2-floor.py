6de4da73cfcb847d2a87322f23193330178a34a2
mkl-74 <mickael1008@gmail.com>
2024-04-23 10:48:19 +0000

    task2 noqa

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""floor which takes a float n as argument and returns the floor of the float."""
+"""floor which takes a float n as argument and returns the floor of the float."""  # noqa
 
 
 def floor(n: float) -> int:
-    """floor which takes a float n as argument and returns the floor of the float."""
+    """floor which takes a float n as argument and returns the floor of the float."""  # noqa
     return int(n)
