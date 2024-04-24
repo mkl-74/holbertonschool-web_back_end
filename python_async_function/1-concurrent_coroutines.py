a8ad2b1b3ca0690f862794bb677f7a6a4106dba8
mkl-74 <mickael1008@gmail.com>
2024-04-24 09:55:14 +0000

    ident-1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -12,9 +12,10 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     delays = [wait_random(max_delay) for _ in range(n)]
     return sorted(await asyncio.gather(*delays))
 
+
 async def wait_random(max_delay: int) -> float:
     """
-    Asynchronous coroutine that waits for a random delay between 0 and max_delay
-    (inclusive) seconds and eventually returns it.
+    Asynchronous coroutine that waits for a random delay between
+    0 and max_delay
     """
     return uniform(0, max_delay)
