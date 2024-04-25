399b160324b473cd1ec974cb272c576101702bb0
mkl-74 <mickael1008@gmail.com>
2024-04-25 07:41:47 +0000

    task2

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""2-measure_runtime.py"""
+import time
+from typing import List
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    start_time = time.time()
+    wait_n(n, max_delay)
+    end_time = time.time()
+    total_time = end_time - start_time
+    return total_time / n
+
+
+if __name__ == "__main__":
+    pass
