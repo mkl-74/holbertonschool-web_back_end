d6c8eb4cdc0c4d31f451ffb3c944447b42bf8cff
mkl-74 <mickael1008@gmail.com>
2024-04-25 09:10:21 +0000

    task2

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -6,24 +6,18 @@ from typing import List
 from time import time
 
 
-
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """
-    Collects 10 random numbers using an asynchronous comprehension over async_generator.
-    """
+    """Collects 10 random numbers"""
     return [number async for number in async_generator()]
 
 
 async def measure_runtime() -> float:
-    """
-    Measures the total runtime of executing async_comprehension four times in parallel.
-    """
+    """Measures the total runtime of executing async_comprehension four times in parallel"""  # noqa
     start_time = time()
 
-    # Execute async_comprehension four times in parallel
     await asyncio.gather(
         async_comprehension(),
         async_comprehension(),
