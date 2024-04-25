3dca9a25d1373ab18fdb4cc3ddc0c5600be56fb1
mkl-74 <mickael1008@gmail.com>
2024-04-25 08:53:06 +0000

    task1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,21 +2,18 @@
 """collect 10 random numbers"""
 
 
-import asyncio  # Add this import statement
-
+import asyncio
 from typing import List
 from random import uniform
 
+
 async def async_generator() -> float:
-    """
-    Coroutine that yields 10 random numbers between 0 and 10 with a 1-second delay.
-    """
+    """Coroutine that yields 10 random numbers between 0 and 10 with a 1-second delay."""  # noqa
     for _ in range(10):
         await asyncio.sleep(1)
         yield uniform(0, 10)
 
+
 async def async_comprehension() -> List[float]:
-    """
-    Collects 10 random numbers using an asynchronous comprehension over async_generator.
-    """
+    """Collects 10 random numbers"""
     return [number async for number in async_generator()]
