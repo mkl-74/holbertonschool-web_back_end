7258d8ea887acd397c1c0bf7239268a7295770eb
mkl-74 <mickael1008@gmail.com>
2024-05-03 09:54:37 +0000

    task0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-"""function named index_range that takes two integer arguments page and page_size"""
+"""function named index_range that takes two
+integer arguments page and page_size"""
 
 
 def index_range(page: int, page_size: int) -> tuple[int, int]:
     """
-    Returns a tuple of start index and end index for a given page and page size.
+    Returns a tuple of start index and end index for a
+    given page and page size.
     """
     # Calculate the start index
-    start_index = (page -1) * page_size
+    start_index = (page - 1) * page_size
 
     # Calculate the end index
     end_index = start_index + page_size
