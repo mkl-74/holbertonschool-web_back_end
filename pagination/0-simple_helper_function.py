8b3a73e7547e621b07ab9bbe0b588048afddfb48
mkl-74 <mickael1008@gmail.com>
2024-05-03 09:56:19 +0000

    task0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -15,14 +15,3 @@ def index_range(page: int, page_size: int) -> tuple[int, int]:
     end_index = start_index + page_size
 
     return start_index, end_index
-
-
-# Test cases
-if __name__ == "__main__":
-    res = index_range(1, 7)
-    print(type(res))
-    print(res)
-
-    res = index_range(page=3, page_size=15)
-    print(type(res))
-    print(res)
