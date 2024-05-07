4eb9e443960ebd507002a3b23f717b389363e952
mkl-74 <mickael1008@gmail.com>
2024-05-07 08:18:28 +0000

    task1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 export default function taskBlock(trueOrFalse) {
     //Variable let
-    let task = false;
+    const task = false;
     let task2 = true;
   
     if (trueOrFalse) {
         //
-        task = true;
-        task2 = false;
+    let task = true;
+    let task2 = false;
     }
   
     return [task, task2];
