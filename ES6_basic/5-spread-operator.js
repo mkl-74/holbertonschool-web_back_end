04d7f01c223309abdebb543f5e6301612a8ee8bd
mkl-74 <mickael1008@gmail.com>
2024-05-07 13:43:48 +0000

    task5

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,10 +1,4 @@
 export default function concatArrays(array1, array2, string) {
-  const concatenatedArrays = array1.concat(array2);
-
-  const concatenatedString = [];
-  for (let i = 0; i < string.length; i++) {
-    concatenatedString.push(string[i]);
-  }
-
-  return concatenatedArrays.concat(concatenatedString);
+  const concatenatedString = [...string];
+  return [...array1, ...array2, ...concatenatedString];
 }
