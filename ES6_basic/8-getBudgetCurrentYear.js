fd6027f4df5f7ab1550b0b9bf99284e2fe21af72
mkl-74 <mickael1008@gmail.com>
2024-05-14 07:46:57 +0000

    task8

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -1,17 +1,12 @@
-// Définition de la fonction getCurrentYear
-function getCurrentYear() {
-    return new Date().getFullYear();
-}
-
 // Fonction getBudgetForCurrentYear
 function getBudgetForCurrentYear(income, gdp, capita) {
-    const currentYear = 2021;
+  const currentYear = 2021;
 
-    return {
-        [`income-${currentYear}`]: income,
-        [`gdp-${currentYear}`]: gdp,
-        [`capita-${currentYear}`]: capita
-    };
+  return {
+    [`income-${currentYear}`]: income,
+    [`gdp-${currentYear}`]: gdp,
+    [`capita-${currentYear}`]: capita,
+  };
 }
 
 // Exportation de la fonction getBudgetForCurrentYear par défaut
