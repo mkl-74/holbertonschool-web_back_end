69ab7fc7827796e823101eaffd97d55fcd2fbdb1
mkl-74 <mickael1008@gmail.com>
2024-05-24 08:22:10 +0000

    indent

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
 export default function cleanSet(set, startString) {
-    if (startString === '' || typeof startString !== 'string') {
-        return '';
-    }
-    return [...set]
-        .filter(value => value.startsWith(startString))
-        .map(value => value.slice(startString.length))
-        .join('-');
+  if (startString === '' || typeof startString !== 'string') {
+    return '';
+  }
+  return [...set]
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
 }
